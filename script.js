dee9988a61515b948fdbe9c0aefec60d69c4bb74
DanielaSantamaria <131626819+DanielaSantamaria@users.noreply.github.com>
2026-06-19 21:44:02 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let boton = document.getElementById("btnMensaje");
+
+boton.addEventListener("click", function(){
+    let mensaje = document.getElementById("mensaje");
+    mensaje.innerText = "Gracias por visitar mi perfil personal.";
+});
